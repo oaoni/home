cbc9c053158f4e90dc728778ef03364fdf7fec41
oaoni <oa.oni7@gmail.com>
2020-10-04 02:45:41 -0400

    edit footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,14 +6,14 @@ const Footer = () => {
   return (
     <footer style={bgStyle} className="mt-auto py-3 text-center">
       {/* <strong> &copy; 2019 </strong>*/}
-      <i className="fas fa-code"></i> with <i className="fas fa-heart"></i> by{" "}
+      <i className="fas fa-code"></i> by{" "}
       <a
         className="badge badge-dark"
         rel="noopener"
-        href="https://github.com/hashirshoaeb"
+        href="https://github.com/oaoni"
         aria-label="My GitHub"
       >
-        Hashir Shoaib
+        Ola Oni
       </a>{" "}
       using <i className="fab fa-react"></i>
       <p>
