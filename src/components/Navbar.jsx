4612fb628edfe63f032c7445c462b8f712b2eeb5
oaoni <oa.oni7@gmail.com>
2020-10-04 02:44:03 -0400

    add side navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import Pdf from "../editable-stuff/resume.pdf";
-import { showBlog, FirstName } from "../editable-stuff/configurations.json";
+import Profile from "../editable-stuff/img/profile.png";
+import { showBlog, FirstName, LastName } from "../editable-stuff/configurations.json";
+
 
 const Navbar = (props) => {
   const [isTop, setIsTop] = useState(true);
@@ -15,6 +17,37 @@ const Navbar = (props) => {
   }, [isTop]);
 
   return (
+
+      <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id="sideNav">
+      <a class="navbar-brand js-scroll-trigger" href="#page-top">
+        <span class="d-block d-lg-none">
+          <a className="navbar-brand" href="#home">
+            {`<${FirstName} ${LastName} />`}
+          </a>
+        </span>
+        <span class="d-none d-lg-block">
+          <img class="img-fluid img-profile rounded-circle mx-auto mb-2" src={Profile} alt="profile pic"/>
+        </span>
+      </a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link js-scroll-trigger" href="#home" id="navlink">Contact</a>
+        </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#aboutme" id="navlink">About</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#projects" id="navlink">Projects</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+
+    /*
     <nav
       className={`navbar navbar-expand-lg fixed-top navbar-light ${
         isTop ? "bg-transparent" : "bg-gradient"
@@ -76,6 +109,7 @@ const Navbar = (props) => {
         </ul>
       </div>
     </nav>
+    */
   );
 };
 
